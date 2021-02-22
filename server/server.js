ae2e76639dc456364fed8169b2d1919767bc45e6
juanmaureira <juanmaureira3@gmail.com>
2021-02-22 13:53:25 -0300

    Actualizacion

--- server/server.js
+++ server/server.js
@@ -18,8 +18,3 @@ app.listen(process.env.PORT, () => {
   console.log('Escuchando en puerto 3000');
 });
 
-// user: juanmaureira
-// pass: 21363660
-// Mongo db URL
-// Base de datos en la nube (mLab): mongodb+srv://juanmaureira:21363660@cluster0.g9a6z.mongodb.net/cafe
-// Base de datos local:  mongodb://localhost:27017/cafe
